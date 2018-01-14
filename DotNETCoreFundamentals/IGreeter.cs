abe45d2aeaa20e9d1f99a2c1d0f2ba6afd4b5465
lekhanhtrinhbp <lekhanhtrinhbp@hotmail.com>
2018-01-14 20:09:17 +0700

    initial commit

---
+++ DotNETCoreFundamentals/IGreeter.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.Extensions.Configuration;
+
+namespace DotNETCoreFundamentals
+{
+    public interface IGreeter
+    {
+        string GetMesageOfTheDay();
+    }
+
+    public class Greeter : IGreeter
+    {
+        private readonly IConfiguration _configuration;
+
+        public Greeter(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+        public string GetMesageOfTheDay()
+        {
+            return _configuration["Greeting"];
+        }
+    }
+}
