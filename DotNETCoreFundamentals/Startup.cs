d6b9c00cc13690c610a8c09964339760615684d4
lekhanhtrinhbp <lekhanhtrinhbp@hotmail.com>
2018-01-15 23:35:39 +0700

    Conventional Routes

--- DotNETCoreFundamentals/Startup.cs
+++ DotNETCoreFundamentals/Startup.cs
@@ -5,6 +5,7 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Routing;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
@@ -18,6 +19,7 @@ namespace DotNETCoreFundamentals
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddSingleton<IGreeter, Greeter>();
+            services.AddMvc();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -28,16 +30,24 @@ namespace DotNETCoreFundamentals
                 app.UseDeveloperExceptionPage();
             }
 
-            //app.UseDefaultFiles();
-            //app.UseStaticFiles();
-
-            app.UseFileServer();
+            app.UseStaticFiles();
+            app.UseMvc(ConfigureRoutes);
 
             app.Run(async (context) =>
             {
                 var greeting = greeter.GetMesageOfTheDay();
-                await context.Response.WriteAsync($"greeting: {greeting}");
+                context.Response.ContentType = "text/plain";
+                await context.Response.WriteAsync("Not Found");
             });
+        }
+
+        private void ConfigureRoutes(IRouteBuilder routeBuilder)
+        {
+            // /Home/Index
+            routeBuilder.MapRoute("Default",
+               "{controller=Home}/{action=Index}/{id?}"
+
+                );
         }
     }
 }
